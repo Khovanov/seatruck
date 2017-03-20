e18f7c13a5f493caf9178f9829e84b70ebd7673f
Khovanov <ubuntu@rails-dev-box>
2017-03-20 04:47:58 +0300

    position factories

--- spec/factories/positions.rb
+++ spec/factories/positions.rb
@@ -1,7 +1,14 @@
 FactoryGirl.define do
+
   factory :position do
+    vessel
+    port
+    open_date "2017-03-01" #Time.zone.now
+  end
+
+  factory :invalid_position, class: 'Position' do
     vessel nil
     port nil
-    open_date "2017-03-20"
+    open_date nil
   end
 end
