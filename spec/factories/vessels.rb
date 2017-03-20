0479ddccafdb27dab2f72288c135c386825c53d7
Khovanov <ubuntu@rails-dev-box>
2017-03-20 04:48:35 +0300

    vessel factories

--- spec/factories/vessels.rb
+++ spec/factories/vessels.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
+  sequence :vessel_title do |n|
+    "Vessel #{n}"
+  end
+
   factory :vessel do
-    title "MyString"
-    hold_capacity 1
+    title { generate :vessel_title }
+    hold_capacity 1000
+  end
+
+  factory :invalid_vessel, class: 'Vessel' do
+    title nil
+    hold_capacity nil
   end
 end
