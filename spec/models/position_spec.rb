ae939440bde01430bf3bdf8d8171f749372cede7
Khovanov <ubuntu@rails-dev-box>
2017-03-20 04:52:07 +0300

    add position model spec

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Position, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations for ...' do
+    it { should belong_to :vessel }
+    it { should belong_to :port }
+  end
+
+  describe 'validates presence of ...' do
+    it { should validate_presence_of :vessel_id }
+    it { should validate_presence_of :port_id }
+    it { should validate_presence_of :open_date }
+  end
 end
