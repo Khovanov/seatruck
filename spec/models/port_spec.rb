1ab1824014a194bb0ba92db9c33dcc08cb4d8772
Khovanov <ubuntu@rails-dev-box>
2017-03-20 04:51:26 +0300

    add port model spec

--- spec/models/port_spec.rb
+++ spec/models/port_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Port, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations for ...' do
+    it { should have_many(:positions).dependent(:destroy) }
+    it { should have_many(:cargos).dependent(:destroy) }
+  end
+
+  describe 'validates presence of ...' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :lat }
+    it { should validate_presence_of :lng }
+  end
 end
