8afeb57c4c65a0b1ce1ff4ce1cee88adc8da2fa4
Khovanov <ubuntu@rails-dev-box>
2017-03-20 04:46:49 +0300

    cargo factories

--- spec/factories/cargos.rb
+++ spec/factories/cargos.rb
@@ -1,8 +1,19 @@
 FactoryGirl.define do
+  sequence :cargo_title do |n|
+    "Cargo #{n}"
+  end
+
   factory :cargo do
-    title "MyString"
+    title { generate :cargo_title }
+    port
+    open_date "2017-03-01" #Time.zone.now
+    capacity 1000
+  end
+
+  factory :invalid_cargo, class: 'Cargo' do
+    title nil
     port nil
-    open_date "2017-03-20"
-    capacity 1
+    open_date nil
+    capacity nil
   end
 end
